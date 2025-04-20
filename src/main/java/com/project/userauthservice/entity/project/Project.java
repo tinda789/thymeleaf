2db0ed7af1c48b90d61dbfc79716e5f4efcd9320
tinda789 <106994095+tinda789@users.noreply.github.com>
2025-04-21 01:02:06 +0700

    lam giao dien lai

---
+++ src/main/java/com/project/userauthservice/entity/project/Project.java
@@ -0,0 +1,54 @@
+package com.project.userauthservice.entity.project;
+
+import com.project.userauthservice.entity.workspace.Workspace;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "projects")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(name = "project_key", unique = true, length = 10)
+    private String projectKey; // VD: PROJ-123
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workspace_id", nullable = false)
+    private Workspace workspace;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private boolean active = true;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
